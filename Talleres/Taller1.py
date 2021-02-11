1876c65a3a1097e26d388e21f8479c64263563cb
JDGC2002 <jhondy141516@hotmail.com>
2021-02-11 08:46:46 -0500

    Taller1 Finalizado

---
+++ Talleres/Taller1.py
@@ -0,0 +1,36 @@
+#Datos
+NumeroA = 12
+NumeroB = 27
+#Números iguales
+areNumerosIguales = NumeroA == NumeroB
+print ("el númeroA es igual al númeroB:", areNumerosIguales)
+print ("---------------------------------------------------")
+#NúmeroA menor que númeroB
+isNumeroA= NumeroA < NumeroB
+print ("el númeroA es menor que el númeroB", isNumeroA)
+print ("---------------------------------------------------")
+#NúmeroA mayor que númeroB
+isNumeroB= NumeroA > NumeroB
+print ("el númeroA es mayor que el númeroB", isNumeroB)
+print ("---------------------------------------------------")
+#Operación suma
+SumaNumeros = NumeroA + NumeroB
+print ("el resultado de la suma es:", SumaNumeros)
+print ("---------------------------------------------------")
+#Operación resta
+RestaNumeros = NumeroA - NumeroB
+print ("el resultado de la resta es:", RestaNumeros)
+print ("---------------------------------------------------")
+#Operación multiplicación
+MultiplicacionNumeros = NumeroA * NumeroB
+print ("el resultado de la multiplicación es:", MultiplicacionNumeros)
+print ("---------------------------------------------------")
+#Operación división
+DivisionNumeros = NumeroA / NumeroB
+print ("el resultado de la división es:", DivisionNumeros)
+print ("---------------------------------------------------")
+#Operación exponencial
+exponencialNumeros = NumeroA ** NumeroB
+print ("el resultado de la operación exponencial es:", exponencialNumeros)
+print ("---------------------------------------------------")
+print ("c:")
