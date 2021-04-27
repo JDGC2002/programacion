b92da414cd5b2522447152d9837d43013838864f
JDGC2002 <jhondy141516@hotmail.com>
2021-04-27 07:24:42 -0500

    Primera clase graficos

---
+++ CLASES/graficos/graficoBarras.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+lenguajes = ['py', 'java', 'dart', 'ts', 'elixir']
+encuesta = [50, 10, 20, 10, 10]
+
+plt.bar(lenguajes, encuesta)
+plt.title ('Lenguajes más usados')
+plt.xlabel ('lenguajes de programación')    
+plt.ylabel ('% de uso de lenguajes')
+plt.savefig ('GraficoLenguajes.png')
+plt.show ()
