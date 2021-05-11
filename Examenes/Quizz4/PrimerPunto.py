d159ca09b7b3779421b21db1b3064ac0601fefe9
JDGC2002 <jhondy141516@hotmail.com>
2021-05-11 07:20:43 -0500

    Primer Punto

---
+++ Examenes/Quizz4/PrimerPunto.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+Snacks = []
+PreciosSnacks = []
+
+for snack in Snacks:
+    Snackk = str(input('Introduzca el snack: '))
+    Snacks.append (Snackk)
+for precio in PreciosSnacks:
+    Precios = float(input('Introduzca el precio: '))
+    PreciosSnacks.append (Precios)
+
